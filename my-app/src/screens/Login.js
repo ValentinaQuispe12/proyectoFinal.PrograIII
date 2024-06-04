99af65a2635093a3b12070fb2df87327b501dcfc
mdipace <mdipace@udesa.edu.ar>
2024-06-04 20:00:15 -0300

    screen de login

--- my-app/src/screens/Login.js
+++ my-app/src/screens/Login.js
@@ -1,12 +1,91 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { auth } from '../firebase/config';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <View>
-        <Text> login </Text>
-      </View>
-    )
-  }
+class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            error: ''
+        }
+    }
+
+    componentDidMount(){
+        auth.onAuthStateChanged((user) => {
+            if(user){
+                console.log('este es el email logueado:', auth.currentUser.email)
+            }
+        })
+    }
+    
+    onSubmit(email, password){
+        if(
+            email === null || email === '' || !email.includes('@')
+        ){
+            this.setState({error: 'el email tiene un formato invalido'})
+            return false
+        }
+        if(
+            password === null || password === ''
+        ){
+            this.setState({error: 'el password no puede ser menor a 8 caracteres'}) // hay q cambiar esto
+            return false
+        }
+        //console.log("usuario logueado exitosamente")
+        
+        auth.signInWithEmailAndPassword(email, password)
+        .then(user => console.log('usuario logueado:', user))
+        //.catch(err => console.log(err))
+        .catch( err => {
+            if(err.code === 'auth/internal-error'){
+                this.setState({error: 'password incorrecta'})
+            }
+        })
+    }
+            
+    render(){
+        return(
+            <View style={styles.container}> 
+                <Text style={styles.text}>Login</Text>
+                {/* email field */}
+                <TextInput style={styles.field}
+                keyboardType = 'email-address'
+                placeholder = 'ingresa tu email'
+                // valor actualizable en tiempo real, (text) es un callback
+                // volver a poner el error para cuando se borre el campo, desaparezca el error viejo
+                onChangeText = {(text) => this.setState({email:text, error: ''})} 
+                value = {this.state.email}
+                />
+
+                {/* password field */}
+                <TextInput style={styles.field}
+                keyboardType = 'default'
+                placeholder = 'ingresa tu password'
+                // secureTextEntry = {true}
+                onChangeText = {(text) => this.setState({password:text, error: ''})}
+                value = {this.state.password}
+                />
+
+                {/* submit 'button' */}
+                <TouchableOpacity 
+                style={styles.button} 
+                // crear la funcion onSubmit() ya que no existe en react native, solo en react
+                onPress ={() => this.onSubmit(this.state.email, this.state.password)}> 
+                    <Text style={styles.text}>Loguearme</Text>
+                </TouchableOpacity>
+                {
+                    this.state.error !== '' ?
+                    <Text>
+                        {this.state.error}
+                    </Text>
+                    :
+                    ''
+                }
+            </View>
+        )
+    }
 }
+
+export default Login
