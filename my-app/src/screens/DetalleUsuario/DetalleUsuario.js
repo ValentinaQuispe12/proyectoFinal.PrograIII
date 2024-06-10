d4c1e2ffa3aa05854b3384fa5288600e89dd51cc
mdipace <mdipace@udesa.edu.ar>
2024-06-10 17:41:33 -0300

    edit de detalleusuario

--- my-app/src/screens/DetalleUsuario/DetalleUsuario.js
+++ my-app/src/screens/DetalleUsuario/DetalleUsuario.js
@@ -6,13 +6,13 @@ class DetalleUsuario extends Component{
     constructor() {
         super();
         this.state = {
-            usuarios: [],
+            usuario: [],
             posteosDelUsuarioSeleccionado: []
         };
     }
 
     componentDidMount(){
-        db.collection('users').onSnapshot(
+        db.collection('users').where('owner', '==', 'mica@gmail.com').onSnapshot(
             docs => {
                 let users = [];
                 docs.forEach( docs => {
@@ -21,12 +21,12 @@ class DetalleUsuario extends Component{
                         data: docs.data()
                     })
                 this.setState({
-                    usuarios: users,
+                    usuario: users,
                 })
                 })
             }
         )
-        db.collection('posteos').where('owner', '==', 'ale@dh.com').onSnapshot(
+        db.collection('posteos').where('owner', '==', 'mica@gmail.com').onSnapshot(
             docs => {
                 let posts = [];
                 docs.forEach( docs => {
@@ -47,10 +47,19 @@ class DetalleUsuario extends Component{
             <View>
                 {/* foto de perfil seleccionado desde la home */}
                 {/* nombre del perfil seleccionado */}
+                <Text>{console.log(this.state.usuario)}</Text>
+
                 {/* bio del perfil seleccionado */}
 
                 {/* lista con los posteos de ese usuario seleccionado 
-                (hacer una validacion de que el usuario seleccionado sea igual al que esta relacionado en la coleccion de posteos)*/}
+                (hacer una validacion de que el usuario seleccionado sea igual al que esta relacionado en la coleccion de posteos)
+                flatlist para recorrer todos los posteos del usuario seleccionado*/}
+                <FlatList
+                data = { this.state.posteosDelUsuarioSeleccionado }
+                keyExtractor = { item => item.id.toString() }
+                // renderItem = { ({item}) => <View>{item.}</View>}
+                renderItem = { ({item}) => console.log(item)}
+                />
             </View>
         )
     }
