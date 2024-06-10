57d0f4b309dffcd3057af5d10ab5d6c7e5c9c03c
mdipace <mdipace@udesa.edu.ar>
2024-06-10 16:50:08 -0300

    empece logica de detalleusuario y cree stack secundario

---
+++ my-app/src/navigation/StackSecundario.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from '../screens/Home/Home';
+import DetalleUsuario from '../screens/DetalleUsuario/DetalleUsuario';
+const Stack = createNativeStackNavigator();
+
+
+export default class StackSecundario extends Component {
+  render() {
+    return (
+    
+        <Stack.Navigator>
+                   <Stack.Screen name="Home" component={Home}  options={{ headerShown: false }} />
+                   <Stack.Screen name="DetalleUsuario" component={DetalleUsuario}  options={{ headerShown: false }} />
+
+        </Stack.Navigator>
+    )
+  }
+}
+
+
