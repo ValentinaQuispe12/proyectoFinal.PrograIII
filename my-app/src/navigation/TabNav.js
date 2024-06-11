02f23016368ed1c3eba766a1046500ef681e4498
mdipace <mdipace@udesa.edu.ar>
2024-06-10 23:55:42 -0300

    termine casi toda logica de detalleusuario

--- my-app/src/navigation/TabNav.js
+++ my-app/src/navigation/TabNav.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 //rutas
-import Home from '../screens/Home/Home'
+import StackSecundario from './StackSecundario'
 import Post from '../screens/Post/Post'
 import Miperfil from '../screens/Miperfil/Miperfil'
+
 // iconos 
 import { Entypo } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
-
-import StackSecundario from './StackSecundario'
 
 const Tab = createBottomTabNavigator()
 
@@ -17,11 +17,9 @@ export default class TabNav extends Component {
   render() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name='home'     component={StackSecundario}    options={{ headerShown: false, tabBarIcon: ()=> <Entypo name="home"       size={24} color="#92CD93" /> }} />
-        <Tab.Screen name='post'     component={Post}    options={{ headerShown: false, tabBarIcon: ()=> <AntDesign name="camerao" size={24} color="#92CD93" />  }} />
-        <Tab.Screen name='miperfil'  component={Miperfil} options={{ headerShown: false, tabBarIcon: ()=> <MaterialCommunityIcons name="human" size={24} color="#92CD93" /> }} />
-
-
+        <Tab.Screen name='home'       component={StackSecundario}     options={{ headerShown: false, tabBarIcon: ()=> <Entypo name="home"       size={24} color="#92CD93" /> }} />
+        <Tab.Screen name='post'       component={Post}                options={{ headerShown: false, tabBarIcon: ()=> <AntDesign name="camerao" size={24} color="#92CD93" />  }} />
+        <Tab.Screen name='miperfil'   component={Miperfil}           options={{ headerShown: false, tabBarIcon: ()=> <MaterialCommunityIcons name="human" size={24} color="#92CD93" /> }} />
       </Tab.Navigator>
     )
   }
