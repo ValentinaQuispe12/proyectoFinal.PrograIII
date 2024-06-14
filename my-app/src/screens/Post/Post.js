be316f3bad34368ca0571fe662ab799b030053ab
ValentinaQuispe12 <vquispe@udesa.edu.ar>
2024-06-14 10:59:32 -0300

    cambios

--- my-app/src/screens/Post/Post.js
+++ my-app/src/screens/Post/Post.js
@@ -1,114 +1,74 @@
+
 import React, { Component } from 'react'
-import { FlatList, TextInput, View, TouchableOpacity, StyleSheet, Text } from 'react-native'
-import { db, auth } from "../../firebase/config"
-import firebase from "firebase"
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import { db, auth } from '../../firebase/config'
+import Camara from '../../components/Camara'
 
-class Comments extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      arrComments: [],
-      comentario: "",
+export default class Post extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pie: '',
+            imgurl: '',
+        }
     }
-  }
 
-  componentDidMount(){
-    console.log('props', this.props)
-    db.collection("posteos")
-    .doc(this.props.route.params.id)
-    .onSnapshot(doc => {
-      console.log('antes del setState, comments', doc.data().comments)
-      this.setState({
-       arrComments: doc.data().comments
-      }, ()=> console.log(this.state))
-    })
-  }
-
-  enviarComentario(comentario){
-    db.collection("posteos")
-    .doc(this.props.route.params.id)
-    .update({
-      comments: firebase.firestore.FieldValue.arrayUnion({
-        owner: auth.currentUser.email,
-        createdAt: Date.now(),
-        comment: comentario
-      })
-    })
-    .catch(err=>console.log(err))
-    
-    this.setState({
-      comentario: ""
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>COMENTA LA FOTO</Text>
-        <FlatList
-          data={this.state.arrComments}
-          keyExtractor={item => item.createdAt.toString()}
-          renderItem={({ item }) => <Text style={styles.comment}>{item.comment}</Text>}
-        />
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Escribi tu comentario"
-            style={styles.input}
-            keyboardType='default'
-            onChangeText={text => this.setState({ comentario: text })}
-            value={this.state.comentario}
-          />
-          <TouchableOpacity style={styles.button} onPress={() => this.enviarComentario(this.state.comentario)}>
-            <Text style={styles.buttonText}>Enviar Comentario</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
-  }
+    onSubmit(pie) {
+        if (pie != '') {
+            db.collection('posteos').add({
+                pie: pie,
+                owner: auth.currentUser.email,
+                createdAt: Date.now(),
+                imageUrl: this.state.imgurl,
+                likes: [],
+                comments: [],
+            })
+                .then((resp) => {
+                    this.setState({
+                        pie: '',
+                        imgurl: ''
+                    },
+                        () => this.props.navigation.navigate('home')
+                    )
+                })
+                .catch((err) => console.log(err))
+        }
+    }
+    actualizarimg(url){
+        this.setState({
+            imgurl: url
+        })
+    }
+    render() {
+        return (
+            <View style={styles.contenedor}>
+                {
+                    this.state.imgurl == '' ?
+                    <Camara actualizarimg={(url)=> this.actualizarimg(url)} /> :
+                    <>
+               
+                <TextInput
+                    placeholder='Escribe un gran pie para tu nuevo post...'
+                    value={this.state.pie}
+                    onChangeText={(text) => this.setState({ pie: text })}
+                    style={styles.input}
+                />
+                <TouchableOpacity onPress={() => this.onSubmit(this.state.pie)}>
+                    <Text> Subi tu post!</Text>
+                </TouchableOpacity>
+                </>
+                 }
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: 'rgb(146, 205, 147)',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#fff',
-    textAlign: 'center',
-  },
-  comment: {
-    padding: 10,
-    marginVertical: 5,
-    backgroundColor: '#92CD93',
-    borderRadius: 5,
-    color: '#fff',
-  },
-  inputContainer: {
-    marginTop: 20,
-  },
-  input: {
-    height: 40,
-    borderColor: '#fff',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    backgroundColor: '#fff',
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: '#92CD93',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
+    contenedor:{
+        flex:1
+    },
+    input: {
+        borderColor: "#93CD93",
+        borderWidth: 1
+    }
 })
-
-export default Comments;
